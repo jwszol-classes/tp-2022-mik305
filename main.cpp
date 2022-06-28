1cedb2536aaaa09a6c66bb5621f874747a96a21a
Mikołaj Turek <95429175+mik305@users.noreply.github.com>
2022-06-28 22:54:05 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,11 @@
-a
+#include "src/window.hpp"
+
+
+int main()
+{
+    window::create();       // stworz okno
+    window::run();          // petla programu do zamkniecia okna
+    window::close();        // wyczysc wszystko co trzeba i zakoncz
+    return 0;
+}
+
