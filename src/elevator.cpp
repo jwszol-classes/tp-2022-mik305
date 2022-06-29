15d177ee8d483495e852cabc46d0df46e080589c
prezes638 <100469610+prezes638@users.noreply.github.com>
2022-06-29 17:27:59 +0200

    Update elevator.cpp

--- src/elevator.cpp
+++ src/elevator.cpp
@@ -6,3 +6,28 @@
 
 #include "imgui.h"
 #include "imgui_internal.h"
+
+const auto MAX_FLOORS = 12;    
+const auto MAX_CAP = 600.0f;    
+const auto PASSANGER_W = 70.0f; 
+const auto MAX_PASS = int(MAX_CAP / PASSANGER_W);
+
+void Elevator::draw()
+{
+    elevatorGo();   
+
+    drawElevator(); 
+    drawMenu();    
+}
+
+
+void Elevator::drawQueueTypeWidgets()
+{
+    ImGui::PushItemWidth(360);  
+    ImGui::LabelText("##l1", "Rodzaj kolejki przy wybieraniu kolejnych pieter");  
+    ImGui::RadioButton("Kolejnosc wcisniecia", &m_queueType, 0);        
+    ImGui::RadioButton("Uwzglednij kierunek ruchu", &m_queueType, 1);   
+    ImGui::RadioButton("Uwzglednij ruch - na parter", &m_queueType, 2);
+    ImGui::NewLine();   
+    ImGui::Separator(); 
+}
