1ca501d374b85fb5f401b6612d0c67b9b8c8ec20
Mikołaj Turek <95429175+mik305@users.noreply.github.com>
2022-06-29 18:08:51 +0200

    Update window.cpp

--- src/window.cpp
+++ src/window.cpp
@@ -16,3 +16,110 @@
 #endif
 
 #include "elevator.hpp"
+
+const auto WIDTH = 1000;    
+const auto HEIGHT = 600;   
+const auto TITLE = "Symulator windy";   
+static GLFWwindow* windowHandler = nullptr;    
+static void glfw_error_callback(int error, const char* description)     
+{
+    fprintf(stderr, "Glfw Error %d: %s\n", error, description); 
+}
+
+
+namespace window
+{
+
+void create()
+{
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit())
+        return;
+
+    // Decide GL+GLSL versions
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+    // GL ES 2.0 + GLSL 100
+    const char* glsl_version = "#version 100";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+#elif defined(__APPLE__)
+    // GL 3.2 + GLSL 150
+    const char* glsl_version = "#version 150";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
+#else
+    // GL 3.0 + GLSL 130
+    const char* glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+#endif
+
+    windowHandler = glfwCreateWindow(WIDTH, HEIGHT, TITLE, NULL, NULL);
+
+    if (windowHandler == NULL)
+        return;
+    glfwMakeContextCurrent(windowHandler);
+    glfwSwapInterval(1); // Enable vsync
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsClassic();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(windowHandler, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+}
+
+void run()
+{
+    Elevator elevator;
+
+    while (!glfwWindowShouldClose(windowHandler))
+    {
+        // Poll and handle events (inputs, window resize, etc.)
+        glfwPollEvents();
+
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        elevator.draw();
+
+        // Rendering
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(windowHandler, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(0.25f, 0.27f, 0.29f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(windowHandler);
+    }
+}
+
+void close()
+{
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(windowHandler);
+    glfwTerminate();
+}
+
+}  // namespace window
