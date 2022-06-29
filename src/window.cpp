91320be799a79b65c2590cdab3a73ef14d3c0684
Mikołaj Turek <95429175+mik305@users.noreply.github.com>
2022-06-29 12:56:37 +0200

    Update window.cpp

--- src/window.cpp
+++ src/window.cpp
@@ -1,1 +1,18 @@
-a
+#include "window.hpp"
+
+#include <functional>
+#include <stdio.h>
+
+#include "imgui.h"
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <GLES2/gl2.h>
+#endif
+#include "glfw3.h" 
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
+#pragma comment(lib, "legacy_stdio_definitions")
+#endif
+
+#include "elevator.hpp"
